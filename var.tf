48892e4d05648174462104f896a3e2c3a2284177
bindumohan222015-create <bindumohan222015@gmail.com>
2026-01-18 17:30:40 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -35,24 +35,6 @@ variable "vmname" {
   default = "tflinuxbin2"
   type    = string
 }
-variable "client_id" {
-  description = "Azure Client ID"
-  type        = string
-}
 
-variable "client_secret" {
-  description = "Azure Client Secret"
-  type        = string
-  sensitive   = true
-}
 
-variable "tenant_id" {
-  description = "Azure Tenant ID"
-  type        = string
-}
-
-variable "subscription_id" {
-  description = "Azure Subscription ID"
-  type        = string
-}
 
